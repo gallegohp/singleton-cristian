4038924e5e4dc6934d8dd29158758395f0807d8b
Camilo Gallego <c.gallego3029@gmail.com>
2026-06-28 18:06:53 -0500

    feat base para el patron singleton

---
+++ src/test/java/com/gallego/singleton/SingletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gallego.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
